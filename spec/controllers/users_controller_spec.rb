bcebd8febd3449b949c6e916651dba1ec340225d
xfun68 <xfun68@gmail.com>
2011-06-27 12:04:13 +0800

    User signup complete

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -41,4 +41,50 @@ describe UsersController do
       response.should have_selector("h1>img", :class => "gravatar")
     end
   end
+
+  describe "POST 'create'" do
+
+    describe "failure" do
+
+      before(:each) do
+        @attr = { :name => "", :email => "", :password => "",
+                  :password_confirmation => "" }
+      end
+
+      it "should not create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector("title", :content => "Sign up")
+      end
+
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template 'new' 
+      end
+    end
+
+    describe "success" do
+
+      before(:each) do
+        @attr = { :name => "xfun68", :email => "xfun68@gmail.com", :password => "xfun68",
+                  :password_confirmation => "xfun68" }
+      end
+
+      it "should create a new user" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by 1
+      end
+
+      it "should have a welcome page" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to the sample app/i
+      end
+    end
+  end
 end
